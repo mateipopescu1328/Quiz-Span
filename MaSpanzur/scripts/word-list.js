4a1e61536d43c1f8d302f78165306216204474e7
Matei Popescu <149295274+mateipopescu1328@users.noreply.github.com>
2023-11-19 02:33:51 +0200

    Add files via upload

---
+++ MaSpanzur/scripts/word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "firewall",
+        hint: "Structură pentru protecția rețelelor."
+    },
+    {
+        word: "bruteforce",
+        hint: "Formă avansată de atac asupra parolelor."
+    },
+    {
+        word: "criptare",
+        hint: "Tehnică de securizare a informațiilor."
+    },
+    {
+        word: "antivirus",
+        hint: "Software care detectează și elimină programele malware."
+    },
+    {
+        word: "https",
+        hint: "Protocol de securitate pentru transferul de date pe internet."
+    },
+    {
+        word: "biometric",
+        hint: "Metodă de autentificare bazată pe amprenta digitală."
+    },
+    {
+        word: "siem",
+        hint: "Software care monitorizează și analizează activitățile suspecte în rețea."
+    },
+    {
+        word: "securizare",
+        hint: "Procesul de protejare a sistemelor împotriva accesului neautorizat."
+    },
+    {
+        word: "steganografie",
+        hint: "Metodă de ascundere a informațiilor prin înglobarea acestora în alte date."
+    },
+    {
+        word: "phishing",
+        hint: "Atac care implică obținerea accesului neautorizat la date prin falsificarea identității."
+    },
+];
